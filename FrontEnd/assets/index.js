ac8d44cedc4fb23874795d2fb7e19383526b9b38
kami90000 <148466110+kami90000@users.noreply.github.com>
2024-04-16 11:35:15 +0200

    Ajout login.js Crétion modal

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -263,3 +263,260 @@ const setDeleteModal = () => {
   addImgBtn.addEventListener("click", setCreateModal);
 
 };
+/**
+ * Sets up the create modal by creating and appending the necessary elements.
+ */
+const setCreateModal = () => {
+
+  const modal       = document.querySelector(".modal");
+  const titleModal  = document.querySelector(".modal h3"); 
+  const line        = document.querySelector(".line");
+
+  const form        = document.createElement("form");
+  const addPhoto    = document.createElement("div");
+  const iconeImg    = document.createElement("i");
+  const labelImg    = document.createElement("label");
+  const btnPreview  = document.createElement("input");
+  const detailsImg  = document.createElement("p");
+  const titleImg    = document.createElement("label");
+  const labelCat    = document.createElement("label");
+  
+  imgInput          = document.createElement("input");
+  inputTxt          = document.createElement("input");
+  select            = document.createElement("select");
+  submitButton      = document.createElement("button");
+
+
+  const options = ["Appartements", "Objets", "Hôtels & restaurants"];
+  for (const optionText of options) {
+    const option = document.createElement("option");
+    option.value = options.indexOf(optionText);
+    option.textContent = optionText;
+    select.appendChild(option);
+  }
+
+  titleModal.nextSibling.remove();
+  line.nextElementSibling.remove();
+
+  modal.id = "create-modal";
+  document.body.classList.add("modal-open");
+  iconeImg.classList.add("fa-regular", "fa-image");
+  addPhoto.classList.add("add-photo");
+  btnPreview.classList.add("btn-preview");
+ 
+  titleModal.textContent    = "Ajout photo";
+  detailsImg.textContent    = "jpg, png : 4mo max";
+
+  labelImg.htmlFor          = "image";
+  imgInput.type             = "file";
+  imgInput.id               = "image";
+  imgInput.htmlFor          = "image";
+  imgInput.accept           = ".jpg, .jpeg, .png";
+  imgInput.style.display    = "none";
+
+  btnPreview.type           = "button";
+  btnPreview.value          = "+ Ajouter photo";
+
+  titleImg.id               = "label";
+  titleImg.htmlFor          = "texte";
+  titleImg.textContent      = "Titre";
+  inputTxt.type             = "text";
+  inputTxt.id               = "texte";
+  inputTxt.name             = "texte";
+
+  labelCat.htmlFor          = "categorie";
+  labelCat.textContent      = "Catégorie";
+  select.id                 = "categorie";
+  select.name               = "categorie";
+
+  submitButton.type         = "submit";
+  submitButton.textContent  = "Valider";
+  submitButton.id           = "submit";
+
+  modal.appendChild(form);
+  form.appendChild(addPhoto);
+  addPhoto.appendChild(iconeImg);
+  addPhoto.appendChild(labelImg);
+  addPhoto.appendChild(imgInput);
+  addPhoto.appendChild(btnPreview);
+  addPhoto.appendChild(detailsImg);
+  form.appendChild(titleImg);
+  form.appendChild(inputTxt);
+  form.appendChild(labelCat);
+  form.appendChild(select);
+  form.appendChild(line);
+  form.appendChild(submitButton);
+
+  imgInput.addEventListener("change", workPreview);
+  btnPreview.addEventListener("click", () => {
+    imgInput.click();
+  });
+ 
+  inputTxt.addEventListener('input', validateForm);
+  select.addEventListener('input', validateForm);
+  imgInput.addEventListener('input', validateForm);
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addWork(inputTxt, select, imgInput);
+  });
+}
+
+// !--------------------------------------- Delete work
+const deleteWork = async (Id) => {
+  const token = localStorage.getItem("userToken");
+  try {
+    const response = await fetch(`http://localhost:5678/api/works/${Id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    });
+
+    if (response.ok) {
+      alert("Le travail a bien été supprimé");
+      createGallery(allWorks);
+      setDeleteModal();
+    } else {
+      const errorMessage = "Une erreur est survenue lors de la suppression du travail.";
+      console.error(errorMessage);
+    }
+  } catch (error) {
+    console.error("Veuillez vous connecter :", error);
+  }
+};
+
+// !--------------------------------------- Preview work
+function displayPreview(file) {
+  const previewContainer = document.querySelector(".add-photo");
+  const previewImg = document.createElement("figure");
+  previewImg.classList.add("img-preview");
+
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    previewImg.innerHTML = `<img src="${reader.result}" alt="Preview">`;
+    previewContainer.innerHTML = "";
+    previewContainer.appendChild(previewImg);
+  });
+
+  reader.readAsDataURL(file);
+}
+
+/**
+ * Generates a preview of a work based on the selected file.
+ *
+ * @param {Event} event - The event object triggered by the user action.
+ */
+function workPreview(event) {
+  const fileInput = event.target;
+  const file = fileInput.files[0];
+
+  const showAlert = () => {
+    alert("Veuillez sélectionner une image au format JPG, JPEG ou PNG, et dont la taille est inférieure à 4 Mo.");
+  };
+
+  if (file && validateWorkFile(file)) {
+    displayPreview(file);
+  } else {
+    showAlert();
+  }
+}
+
+/**
+ * Validates if a given file is a valid work file.
+ *
+ * @param {Object} file - The file object to be validated.
+ * @return {boolean} Returns true if the file is a valid work file, otherwise returns false.
+ */
+const validateWorkFile = (file) => {
+  const allowedTypes = new Set(["image/jpeg", "image/jpg", "image/png"]);
+  const maxFileSize = 4 * 1024 * 1024;
+
+  return allowedTypes.has(file.type) && file.size <= maxFileSize;
+};
+
+// !--------------------------------------- Add work
+
+const addWork = (inputTxt, select, imgInput) => {
+  const formData = new FormData();
+  formData.append("title", inputTxt.value);
+  formData.append("category", select.value);
+  formData.append("image", imgInput.files[0]);
+
+  const token = localStorage.getItem("userToken");
+
+  fetch("http://localhost:5678/api/works", {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${token}`,
+    },
+    body: formData,
+  })
+    .then(async (res) => {
+      if (res.ok) {
+        alert("Le travail a bien été ajouté");
+
+        const newWorkResponse = await res.json();
+        allWorks.push(newWorkResponse);
+        createGallery(allWorks);
+        setDeleteModal();
+      }
+    })
+    .catch((error) => {
+      console.error("Erreur lors de l'ajout du travail :", error);
+    });
+};
+
+
+// !--------------------------------------- Validate form
+function validateForm() {
+  if (inputTxt && inputTxt.value !== "" && select.value !== "option" && imgInput.files.length > 0) {
+    submitButton.style.background = "#1D6154";
+    submitButton.disabled = false;
+    submitButton.style.cursor = "pointer";
+  } else {
+    submitButton.disabled = true;
+    submitButton.style.background = "#A7A7A7";
+    submitButton.style.cursor = "auto";
+  }
+}
+// !--------------------------------------- Log in
+const login = () => {
+  const log = document.getElementById("log");
+
+  const userToken = localStorage.getItem("userToken");
+
+  if (userToken) {
+    log.innerText = "logout";
+
+    log.addEventListener("click", (event) => {
+      localStorage.removeItem("userToken");
+      localStorage.removeItem("userId");
+      log.innerText = "login";
+
+      banner.remove();
+      modifyBtn.remove();
+
+      alert("Vous êtes déconnecté");
+    });
+  } else {
+    log.innerText = "login";
+  }
+};
+
+// !--------------------------------------- Initialization
+/**
+ * Initializes the application by fetching works and categories.
+ * Checks if the user is logged in and displays the appropriate page.
+ *
+ * @return {Promise<void>} - A promise that resolves when the initialization is complete.
+ */
+const initialize = async () => {
+  await getWorks();
+  await getCategories();
+  const userToken = localStorage.getItem("userToken");
+  userToken ? displayAdmin() : displayUser();
+};
+
+initialize();
---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// !----------------------------- Constantes
+const submit = document.getElementById("submit");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+// !----------------------------- Functions
+/**
+ * Saves the provided token to the user's local storage.
+ *
+ * @param {string} token - The token to be saved.
+ */
+function saveToken(token) {
+    localStorage.setItem("userToken", token);
+}
+
+/**
+ * Sends the user ID to the server for authentication.
+ *
+ * @param {type} paramName - description of parameter
+ * @return {type} description of return value
+ */
+function sendId() {
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email: email.value,
+            password: password.value
+        })
+    })
+    .then((res) => {
+        if (res.ok) {
+            return res.json();
+        } else {
+            throw new Error("Erreur dans l’identifiant ou le mot de passe");
+        }
+    })
+    .then((data) => {
+        const userdata = data.token;
+        saveToken(userdata);
+        document.location.href = "index.html";
+    })
+    .catch((error) => {
+        document.querySelector(".errorLogin").innerHTML = error.message;
+    });
+}
+
+// !----------------------------- Événement
+submit.addEventListener("click", (event) => {
+    event.preventDefault();
+    sendId();
+});
